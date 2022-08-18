d17acc72d1f07b21fdef7de961f48a3376eec8ac
Jonathan Costa <jonathancosta746@gmail.com>
2022-08-18 05:35:16 -0300

    adicionado responsividade

--- src/Components/GameOver.js
+++ src/Components/GameOver.js
@@ -2,8 +2,8 @@ import './GameOver.css'
 
 const GameOver = ({retry, score}) => {
   return (
-    <div>
-      <h1>Fim de Jogo!</h1>
+    <div className="gameover">
+      <h1 >Fim de Jogo!</h1>
       <h2>A sua pontuação foi: <span>{score}</span></h2>
       <button onClick={retry}>Reiniciar</button>
     </div>
